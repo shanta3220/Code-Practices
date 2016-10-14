5ae1f3b5fe60784eab67ca10312832cacdaadd8e
shanta <evilevil4991@yahoo.com>
2016-10-14 19:07:40 +0600

    Create D. Box Sorting.cpp

---
+++ ACM-ICPC-2016-MOCK/D. Box Sorting.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+	int max, t, n, j =1;
+	string s;
+	cin >> t;
+	while(t--){
+	  cin >> n;
+      int boxes[n];
+   	  map<int, int> box;
+	  max = 0;
+	  for (int i = 0; i < n; ++i){
+            cin >> boxes[i];
+    	    box[boxes[i]]+=1;
+    	    if(max < box[boxes[i]])
+    	      max = box[boxes[i]];
+
+	    }
+	    cout <<"Case "<< j++ <<": ";
+	   
+	          cout << max << "\n";
+	}
+	return 0;
+}
